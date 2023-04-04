d9714ecc4cf6c2cb3c98154f67242debc1057dbc
hakan.ozcan <hakan.ozcan@SAWRNTGOIT9>
2023-04-04 08:43:45 +0300

    Add project files.

---
+++ MongoDbProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // MongoDB veritabanına bağlanma
+        var client = new MongoClient("mongodb://localhost:27017/");
+
+        // Veritabanı ve koleksiyon oluşturma
+        var database = client.GetDatabase("hakandatabase");
+        var collection = database.GetCollection<BsonDocument>("customers");
+
+        // Veri ekleme
+        var data = new BsonDocument { { "name", "Hakan" }, { "address", "Maltepe" } };
+        collection.InsertOne(data);
+
+        // Veri ekleme sonucunu yazdırma
+        Console.WriteLine(data["_id"]);
+    }
+}
